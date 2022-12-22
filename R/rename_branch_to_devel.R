5a11764c53a6e74d146201ea1f848b492a1de47f
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-12-22 20:00:26 +0000

    add update_local_repos to sync from GH

--- R/rename_branch_to_devel.R
+++ R/rename_branch_to_devel.R
@@ -290,3 +290,72 @@ branch_all_repos <- function(
         is_bioc_pkg = FALSE
     )
 }
+
+#' Update local repositories after GitHub branch rename
+#'
+#' This function works _only_ after renaming branches on GitHub from `master` to
+#' `devel`. It updates local folders so that branches are renamed to `devel`
+#' locally. It also ensures that the `HEAD` is pointing to the new remote
+#' location, i.e., `origin/devel` (see `gert::git_remote_info()$head`).
+#'
+#' @details Note that the function queries the GitHub API to discover
+#'   repositories whose default branches are set to `devel`. The function then
+#'   matches those repository names with local folder names under `basedir`
+#'   and performs the necessary steps to rename the branch and set the
+#'   `HEAD` to `origin/devel`.
+#'
+#' @param basedir character(1) The base directory where all packages /
+#'   repositories exist for the user
+#'
+#' @param username character(1) The GitHub username used in the query to check
+#'   default packages
+#'
+#' @inheritParams gert::git_branch_move
+#'
+#' @export
+update_local_repos <- function(
+    basedir, username, new_branch = "devel", set_upstream = "origin/devel"
+) {
+    repos <- get_user_github_repos(username = username)
+    repos <- repos[repos == new_branch]
+    folders <- list.dirs(basedir, recursive = FALSE)
+    fnames <- basename(folders)
+    matching <- intersect(folders, names(repos))
+    if (!length(matching))
+        stop("No local folders in 'basedir' to update")
+
+    mapply(
+        FUN = .update_local_repo,
+        folder = matching,
+        MoreArgs = list(
+            basedir = basedir,
+            new_branch = new_branch,
+            set_upstream = set_upstream
+        ),
+        SIMPLIFY = FALSE
+    )
+}
+
+.update_local_repo <- function(
+        basedir, folder, new_branch = "devel", set_upstream = "origin/devel"
+) {
+    old_wd <- setwd(file.path(basedir, folder))
+    on.exit({ setwd(old_wd) })
+    if (git_branch_exists(new_branch))
+        return(git_branch_checkout(new_branch))
+    from_branch <- git_branch()
+    git_branch_move(
+        branch = from_branch, new_branch = new_branch, repo = I(".")
+    )
+    if (!.is_remote_github())
+        stop("'origin' remote should be set to GitHub")
+    git_fetch(remote = "origin")
+    system2("git", "remote set-head origin -a")
+    git_branch_set_upstream(set_upstream)
+}
+
+.is_remote_github <- function(remote = "origin") {
+    remotes <- git_remote_list()
+    remote_url <- unlist(remotes[remotes[["name"]] == remote, "url"])
+    grepl("github", remote_url, ignore.case = TRUE)
+}

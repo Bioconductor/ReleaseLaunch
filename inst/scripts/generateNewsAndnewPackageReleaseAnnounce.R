59b82ccd84c2f85bd5971a781df0c505b546def2
lshep <lori.shepherd@roswellpark.org>
2024-05-09 10:25:09 -0400

    Minor updates

--- inst/scripts/generateNewsAndnewPackageReleaseAnnounce.R
+++ inst/scripts/generateNewsAndnewPackageReleaseAnnounce.R
@@ -5,29 +5,38 @@ library(biocViews)
 ## to manually add)
 
 
-softNEWS = getPackageNEWS("3.14", "3.15", repo="bioc", srcdir="/home/shepherd/BioconductorPackages/Software")
-dataNEWS = getPackageNEWS("3.14", "3.15", repo="data/experiment")
-WFNEWS = getPackageNEWS("3.14", "3.15",repo="workflows",srcdir="/home/shepherd/BioconductorPackages/Workflows")
+softNEWS = getPackageNEWS("3.18", "devel", repo="bioc", srcdir="/home/lorikern/BioconductorPackages/SoftwarePkg")
+dataNEWS = getPackageNEWS("3.18", "devel", repo="data/experiment")
+WFNEWS = getPackageNEWS("3.18", "devel",repo="workflows",srcdir="/home/lorikern/BioconductorPackages/WorkflowPkg")
 
 printNEWS(softNEWS, "softwareNews.md", relativeLink=TRUE, overwrite=TRUE)
 printNEWS(dataNEWS, "dataNews.md", relativeLink=TRUE, overwrite=TRUE)
 printNEWS(WFNEWS, "workflowNEWS.md", relativeLink=TRUE, overwrite=TRUE)
 
+rmarkdown::render("softwareNews.md")
+rmarkdown::render("dataNews.md")
+rmarkdown::render("workflowNEWS.md")
 
 
+newSoft = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch = "RELEASE_3_19")
+rmSoft = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18",currBranch="RELEASE_3_19", status="removed")
+deprecatedSoft = setdiff(biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch = "devel",status="removed"),rmSoft)
 
+newData = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch = "RELEASE_3_19", manifest="data-experiment.txt")
+rmData = biocViews:::getPackageTitles(prevBranch="RELEASE_3_18", currBranch="RELEASE_3_19", manifest="data-experiment.txt", status="removed")
+deprecatedData = setdiff(biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch ="devel",manifest="data-experiment.txt", status="removed"), rmData)
 
-newSoft = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_14", currBranch = "RELEASE_3_15")
-rmSoft = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_14",currBranch="RELEASE_3_15", status="removed")
-deprecatedSoft = setdiff(biocViews:::getPackageTitles(prevBranch = "RELEASE_3_15", currBranch = "master",status="removed"),rmSoft)
+newWork = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch = "RELEASE_3_19", manifest="workflows.txt")
+rmWork = biocViews:::getPackageTitles(prevBranch="RELEASE_3_18", currBranch="RELEASE_3_19", manifest="workflows.txt", status="removed")
+deprecatedWork = setdiff(biocViews:::getPackageTitles(prevBranch ="RELEASE_3_18", currBranch = "devel",manifest="workflows.txt",status="removed"), rmWork)
 
-newData = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_14", currBranch = "RELEASE_3_15", manifest="data-experiment.txt")
-rmData = biocViews:::getPackageTitles(prevBranch="RELEASE_3_14", currBranch="RELEASE_3_15", manifest="data-experiment.txt", status="removed")
-deprecatedData = setdiff(biocViews:::getPackageTitles(prevBranch = "RELEASE_3_14", currBranch ="master",manifest="data-experiment.txt", status="removed"), rmData)
+newBook = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch = "RELEASE_3_19", manifest="books.txt")
+rmBook = biocViews:::getPackageTitles(prevBranch="RELEASE_3_18", currBranch="RELEASE_3_19", manifest="books.txt", status="removed")
+deprecatedBook = setdiff(biocViews:::getPackageTitles(prevBranch ="RELEASE_3_18", currBranch = "devel",manifest="books.txt",status="removed"), rmBook)
 
-newWork = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_14", currBranch = "RELEASE_3_15", manifest="workflows.txt")
-rmWork = biocViews:::getPackageTitles(prevBranch="RELEASE_3_14", currBranch="RELEASE_3_15", manifest="workflows.txt", status="removed")
-deprecatedWork = setdiff(biocViews:::getPackageTitles(prevBranch ="RELEASE_3_14", currBranch = "master",manifest="workflows.txt",status="removed"), rmWork)
+newAnn = biocViews:::getPackageTitles(prevBranch = "RELEASE_3_18", currBranch = "RELEASE_3_19", manifest="data-annotation.txt")
+rmAnn = biocViews:::getPackageTitles(prevBranch="RELEASE_3_18", currBranch="RELEASE_3_19", manifest="data-annotation.txt", status="removed")
+deprecatedAnn = setdiff(biocViews:::getPackageTitles(prevBranch ="RELEASE_3_18", currBranch = "devel",manifest="data-annotation.txt",status="removed"), rmAnn)
 
 
 sink("PackageOverview.txt")
@@ -42,12 +51,22 @@ cat("\n\ndeprecatedData:\n",length(deprecatedData), "\n\n",paste(sort(deprecated
 cat("\n\nnewWorkflow:\n",length(newWork), "\n\n",paste(sort(newWork), collapse=", "))
 cat("\n\nremovedWorkflow:\n", length(rmWork), "\n\n",paste(sort(rmWork), collapse=", "))
 cat("\n\ndeprecatedWorkflow:\n", length(deprecatedWork), "\n\n",paste(sort(deprecatedWork), collapse=", "))
+
+cat("\n\nnewAnnotation:\n",length(newAnn), "\n\n",paste(sort(newAnn), collapse=", "))
+cat("\n\nremovedAnnotation:\n", length(rmAnn), "\n\n",paste(sort(rmAnn), collapse=", "))
+cat("\n\ndeprecatedAnnotation:\n", length(deprecatedAnn), "\n\n",paste(sort(deprecatedAnn), collapse=", "))
+
+cat("\n\nnewBook:\n",length(newBook), "\n\n",paste(sort(newBook), collapse=", "))
+cat("\n\nremovedBook:\n", length(rmBook), "\n\n",paste(sort(rmBook), collapse=", "))
+cat("\n\ndeprecatedBook:\n", length(deprecatedBook), "\n\n",paste(sort(deprecatedBook), collapse=", "))
+
 sink()
 
 biocViews:::getPackageDescriptions(newSoft, "newSoft.md", relativeLink=TRUE)
 biocViews:::getPackageDescriptions(newData, "newData.md", relativeLink=TRUE)
 biocViews:::getPackageDescriptions(newWork, "newWork.md", relativeLink=TRUE)
-
+biocViews:::getPackageDescriptions(newAnn, "newAnn.md", relativeLink=TRUE)
+biocViews:::getPackageDescriptions(newBook, "newBook.md", relativeLink=TRUE)
 
 
 

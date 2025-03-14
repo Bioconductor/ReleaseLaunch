1995d4fcc3416bcc565034b62ed06281188ac09c
LiNk-NY <marcel.ramosperez@roswellpark.org>
2025-03-14 18:09:09 +0000

    add clone_repos helper function
    - update NAMESPACE

---
+++ R/clone_repos.R
@@ -0,0 +1,55 @@
+
+#' @export
+clone_repos <- function(
+    packages,
+    dest_dir,
+    username,
+    org = username,
+    set_upstream = "origin/devel",
+    restart_from = character(0L),
+    BPPARAM = NULL
+) {
+    stopifnot(
+        isScalarCharacter(repos_dir) && dir.exists(repos_dir),
+        isScalarCharacter(set_upstream)
+    )
+    if (missing(username) && missing(org))
+        stop("Either 'username' or 'org' must be provided")
+
+    if (isScalarCharacter(restart_from)) {
+        indx <- which(packages == restart_from)
+        if (!length(indx))
+            stop("Package '", restart_from, "' not found in 'repos_dir'")
+        packages <- packages[indx:length(packages)]
+    }
+
+    mapply(
+        FUN = clone_repo,
+        package = packages,
+        MoreArgs = list(
+            dest_dir = dest_dir,
+            set_upstream = set_upstream,
+            org = org
+        )
+    )
+}
+
+#' @export
+clone_repo <- function(
+    package,
+    dest_dir,
+    org,
+    set_upstream = "origin/devel"
+) {
+    dest_dir <- file.path(dest_dir, package)
+    bioc_slug <- .get_bioc_slug(package)
+    gh_slug <- .get_gh_slug(org = org, package_name = package)
+    if (.repo_exists(pkg = package, org = org)) {
+        gert::git_clone(gh_slug, path = dest_dir)
+        old <- setwd(dest_dir)
+        on.exit(setwd(old))
+        gert::git_remote_add(bioc_slug, name = "upstream")
+    } else {
+        gert::git_clone(bioc_slug)
+    }
+}
